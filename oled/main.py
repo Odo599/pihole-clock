1b9412f3755bca5e28b6f9a34623fb507457d019
Odo599 <owen00064@gmail.com>
2026-01-25 15:14:33 +1100

    added main loop

--- oled/main.py
+++ oled/main.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 import display
 import display.preview as preview
@@ -15,7 +16,6 @@ state = libs.AlarmState()
 fb = display.init_display()
 buttons = input.init_buttons()
 
-# ui testing
 def draw_ui(draw):
     libs.draw_time(draw,state)
     libs.draw_alarm_bells(draw,state)
@@ -27,18 +27,9 @@ def update_screen():
 def check_buttons():
     libs.check_alarms(buttons.poll(), state)
 
-update_screen()
-
-# testing
-libs.pressAlarm2(buttons)
-check_buttons()
-update_screen()
-
-libs.startHoldAlarm2(buttons)
-check_buttons()
-update_screen()
-
-libs.endHoldAlarm2(buttons)
-check_buttons()
-update_screen()
+# main loop
+while True:
+    check_buttons()
+    update_screen()
+    time.sleep(0.01)
 
