e915bcb8d7e772c42020328432e13bd2138f7ae1
Odo599 <owen00064@gmail.com>
2026-01-25 15:14:58 +1100

    fixed

--- oled/display/preview.py
+++ oled/display/preview.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import time
 
-def save_frame(framebuffer, path="preview.png"):
+def save_frame(framebuffer):
     framebuffer.image.show()
 
 def timed_preview(device, seconds=5):
