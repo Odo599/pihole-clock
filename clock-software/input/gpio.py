4c1803c1369192f5309148ab95f47fa9aed2fe4a
Odo599 <owen00064@gmail.com>
2026-01-30 10:41:50 +1100

    added button release support

--- clock-software/input/gpio.py
+++ clock-software/input/gpio.py
@@ -9,6 +9,13 @@ def setup_gpio(buttonManager: ButtonManager):
     press_minute = lambda : buttonManager.press("MINUTE")
     press_alarm1 = lambda : buttonManager.press("ALARM1")
     press_alarm2 = lambda : buttonManager.press("ALARM2")
+    release_play   = lambda : buttonManager.release("PLAY")
+    release_vdown  = lambda : buttonManager.release("VDOWN")
+    release_vup    = lambda : buttonManager.release("VUP")
+    release_hour   = lambda : buttonManager.release("HOUR")
+    release_minute = lambda : buttonManager.release("MINUTE")
+    release_alarm1 = lambda : buttonManager.release("ALARM1")
+    release_alarm2 = lambda : buttonManager.release("ALARM2")
 
     PLAY_BUTTON.when_activated = press_play
     VDOWN_BUTTON.when_activated = press_vdown
@@ -17,4 +24,12 @@ def setup_gpio(buttonManager: ButtonManager):
     MINUTE_BUTTON.when_activated = press_minute
     ALARM1_BUTTON.when_activated = press_alarm1
     ALARM2_BUTTON.when_activated = press_alarm2
+
+    PLAY_BUTTON.when_deactivated = release_play
+    VDOWN_BUTTON.when_deactivated = release_vdown
+    VUP_BUTTON.when_deactivated = release_vup
+    HOUR_BUTTON.when_deactivated = release_hour
+    MINUTE_BUTTON.when_deactivated = release_minute
+    ALARM1_BUTTON.when_deactivated = release_alarm1
+    ALARM2_BUTTON.when_deactivated = release_alarm2
 
